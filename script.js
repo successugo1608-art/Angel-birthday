1b0cdeea69993a1c5d1b0cf085b7012ba2f6eace
successugo1608-art <successugo1608@gmail.com>
2026-06-29 18:25:22 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,183 @@
+function checkpassword() {
+    let answer = document.getElementById("password").value;
+
+    alert(answer);
+
+    if (answer === "2021") {
+        window.location.href = "./universe.html";
+    } else {
+        document.getElementById("message").innerHTML = "Wrong ANSWER😭";
+    }
+}
+function MEMORY1 () { 
+    document.getElementById ("memory").innerHTML=`    <h2> REMEMBER WHEN WE FIRST MET? <h2>
+    <div class="memory-grid"> 
+     <div class="photo"> 
+     <img src = "pic14.jpg" width="400"> 
+     <p> THE ONLY PIC I HAVE OF BOTH OF US BTW😞</p>
+    </div>
+    <div class="photo">
+    <img src= "pic1.jpg" width="400">
+    <p> THE DAY WE WENT TO MILLICENT'S HOUSE😏</p>
+    </div>
+    <div class="photo">
+    <img src= "pic16.jpg" width="400"> 
+    <p> I REALLY LOVE THIS PICTURE♥️</p>
+    </div>
+    <div class="photo">
+
+    <video controls>
+        <source src="vid3 - Trim.mp4" type="video/mp4">
+    </video>
+
+    <p> REMEMBER WHEN YOU PULLED THIS BULLSHIT </p>
+
+     </div>
+    `;} 
+function MEMORY2 () {
+    document.getElementById("memory").innerHTML=`      <h2>"MY BABY IS ALL GROWN UP😭😭"</h2> 
+    <div class="memory-grid"> 
+    <div class="photo">
+    <img src = "pic5.jpg" width="400"> 
+    <p> SO CUTE!!! </p> 
+    </div>
+    <div class="photo">
+    <img src = "pic11.jpg" width="400"> 
+    <p> YOUR TINY SMILE>>>> </p>
+    </div>
+    <div class="photo">
+    <img src = "pic25.jpg" width="400"> 
+    <p> 🙃</p>
+    </div>
+    <div class="photo">
+    <img src = "pic17.jpg" width="400"> 
+    <p>😏</p>
+    </div>
+    <div class="photo">
+    <img src = "pic13.jpg" width="400"> 
+    <p> YOUR BIG ASS GELE </p>
+    </div>
+    <div class="photo">
+    <img src = "pic20.jpg" width="400"> 
+    <p> IM NEVER DELETING THIS SO DON'T ASK🙄</p>
+    </div>
+    `;}
+function MEMORY3 () { 
+    document.getElementById("memory").innerHTML=`    <h2> you're the fucking best!!!</h2> 
+    <div class="memory-grid"> 
+    <div class="photo">
+    <img src = "pic8.jpg" width="400"> 
+    <p> YOU'RE HAIR WAS HALF LOOSED HERE</p>
+    </div>
+    <div class="photo">
+    <img src = "pic10.jpg" width="400"> 
+    <p> DRUNKARD</p>
+    </div>
+    <div class="photo">
+    <img src = "pic15.jpg" width="400"> 
+    <p> MY ALL TIME FAVOURITE</p>
+    </div>
+    <div class="photo">
+    <img src = "pic7.jpg" width="400"> 
+    <p> 😭😭</p>
+    </div>
+    <div class="photo">
+    <img src ="pic21.jpg" width="400"> 
+    <p> HALF LOOSED HAIR AGAIN </p>
+    </div>
+    <div class="photo">
+    <img src = "pic6.jpg" width="400"> 
+    <p> THE AMOUNT OF EATING PICS I HAVE OF YOU EHN</p>
+    </div>
+    `;}
+function MEMORY4 () {
+    document.getElementById("memory").innerHTML=`    <h2> YOU'RE THE MOST BEUTIFUL GIRL EVER (with Gay intentions) </h2>
+    <div class="memory-grid">
+    <div class="photo">
+    <img src = "pic2.jpg" width="400"> 
+    <p> IDK WHY YOU DONT LIKE THESE PHOTOS THEY'RE THE PRITTIEST EVER </p>
+    </div>
+    <div class="photo">
+    <img src = "pic4.jpg" width="400"> 
+    <p> 😏 </p>
+    </div>
+    <div class="photo">
+    <img src = "pic3.jpg" width="400"> 
+    <p> YOU AND THIS FILTER THAT TIME</p>
+    </div>
+    <div class="photo">
+    <img src = "pic12.jpg" width="400"> 
+    <p> I REALLY LIKED THIS ONE </p>
+    </div>
+    <div class="photo">
+    <img src = "pic22.jpg" width="400"> 
+    <p> IMAGINE THAT SMILING MOON EMOJI, I NO GET AM FOR MY LAPTOP</p>
+    </div>
+    <div class="photo">
+
+    <video controls>
+        <source src="vid 2.mp4" type="video/mp4">
+    </video>
+
+    <p> 😜😜😜 </p>
+
+     </div>
+    `;}
+function MEMORY5 () {
+    document.getElementById("memory").innerHTML=`   <h2> MORE OF YOU'RE PICS COS IT'S THE ONLY THING ON MY PHONE</h2>
+    <div class="memory-grid">
+    <div class="photo"> 
+    <img src = "pic18.jpg" width="400"> 
+    <p> -</p>
+    </div>
+    <div class="photo">
+    <img src = "pic19.jpg" width="400">
+    <p> -</p>
+    </div>
+    <div class="photo">
+    <img src = "pic23.jpg" width="400"> 
+    <p> -</p>
+    </div>
+    <div class="photo">
+    <img src = "pic24.jpg" width="400"> 
+    <p> -</p>
+    </div>
+    <div class="photo">
+    <img src = "pic26.jpg" width="400"> 
+    <p> - </p>
+    </div>
+    <div class="photo">
+
+    <video controls>
+        <source src="vid 1.mp4" type="video/mp4">
+    </video>
+
+    <p> THE END:) </p>
+
+    </div>
+    `} 
+function BIRTHDAYSTAR(){
+
+document.getElementById("memory").innerHTML = `
+
+<div class="birthday-card">
+
+<h1>Happy Birthday, Angel 💜</h1>
+
+<p>
+Dear angel, 
+On this day, 19 years ago, marked the birth of someone so extra-ordinary, so remarkable, it still puzzles me how something so beautiful exists in this goddamned planet, I could say I love you but mere words would not enough, 5 years ago you came into my life and quite violently pulled me out of my shell, I honestly don’t know where I would be without you, It never really made sense why you would like someone like me, 
+you were and still are the perfect embodiment of a ray of sunshine, your smile, your laugh your fuckass shiny forehead, day after day you made dorben bearable, you help me through thing you don’t even know about, just by existing.
+</p> 
+
+<p>
+When you went to university, it scared me because I thought you’d find someone better and move on, but – to my surprise, honestly- you didn’t, and even though we didn’t talk every day you were still there, you’re Everything. I might not really believe in God, but if there is anyone God sent in my life, it's you. Many people spend their whole lives trying to find their soulmate, and I found mine at twelve. If sending you my way was the only thing God ever did for me id be content, because you are more than enough.
+So, thank you, Angel Efe Rubina Obaraye, for making my shit storm of a life worth living, till death do us part Bitch. 
+
+</p>
+
+</div>
+
+`;
+
+}
